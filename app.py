4bee8cba1530c3249fdc90d0dd5ceec894d5c464
Mark Berger <mark.berger.j@gmail.com>
2013-05-03 10:01:09 -0700

    Changed font, navbar, and added background image

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask 
+from flask import render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+@app.route('/calendar')
+def calendar():
+	return render_template('calendar.html')
+
+def main():
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+	main()
