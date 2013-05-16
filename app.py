b595920e61eb03b34978b6a1b829efa60dda7d4d
Mark Berger <mark.berger.j@gmail.com>
2013-05-16 12:11:58 -0700

    Added emails to Contact Us and changed link style

--- app.py
+++ app.py
@@ -8,9 +8,25 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
 @app.route('/calendar')
 def calendar():
 	return render_template('calendar.html')
+
+@app.route('/projects')
+def projects():
+	return render_template('projects.html')
+
+@app.route('/corporate')
+def corporate():
+	return render_template('corporate.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
 
 def main():
 	port = int(os.environ.get('PORT', 5000))
