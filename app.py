faecd55b2472a6d90b4a6729efde51e3bafabd6c
Mark Berger <mark.berger.j@gmail.com>
2013-05-17 00:14:31 -0700

    Changed html so that Frozen-Flask works and wrote a function to freeze the site

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from flask import render_template
+
 import os
 
 app = Flask(__name__)
@@ -8,29 +9,34 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
 	return render_template('about.html')
 
-@app.route('/calendar')
+@app.route('/calendar.html')
 def calendar():
 	return render_template('calendar.html')
 
-@app.route('/projects')
+@app.route('/projects.html')
 def projects():
 	return render_template('projects.html')
 
-@app.route('/corporate')
+@app.route('/corporate.html')
 def corporate():
 	return render_template('corporate.html')
 
-@app.route('/contact')
+@app.route('/contact.html')
 def contact():
 	return render_template('contact.html')
 
 def main():
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
+
+def build():
+	from flask_frozen import Freezer
+	freezer = Freezer(app)
+	freezer.freeze()
 
 if __name__ == '__main__':
 	main()
