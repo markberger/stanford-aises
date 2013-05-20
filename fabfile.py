b4deb7f499c46e3a4bd8eeacb9ae553842c7a82c
Mark Berger <mark.berger.j@gmail.com>
2013-05-20 15:52:15 -0700

    Created a fabfile to freeze and deploy the site

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import local, env, run, cd, put
+from flask_frozen import Freezer
+from main import app
+from fab-settings import *
+
+env.user = USER
+env.hosts = HOSTS
+
+def freeze():
+	print("Building static version of app...")
+	freezer = Freezer(app)
+	freezer.freeze()
+	print("Freezing complete.")
+
+def deploy(servName="test"):
+	if servName == "test":
+		print "Deploying to stanford-aises.herokuapp.com"
+		local("git push heroku master")
+	if servName == "production":
+		print "Deploying to Stanford AFS"
+		freeze()
+		print "Uploading files to AFS"
+		put("build/*", "~/aises/WWW")
+
