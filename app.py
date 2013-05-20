e257d8ca6ddd5d07910fd835533ea0d843559035
Mark Berger <mark.berger.j@gmail.com>
2013-05-20 14:50:14 -0700

    Refactored app.py

--- app.py
+++ app.py
@@ -1,37 +1,6 @@
 from flask import Flask 
-from flask import render_template
-
-import os
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/about.html')
-def about():
-	return render_template('about.html')
-
-@app.route('/calendar.html')
-def calendar():
-	return render_template('calendar.html')
-
-@app.route('/projects.html')
-def projects():
-	return render_template('projects.html')
-
-@app.route('/corporate.html')
-def corporate():
-	return render_template('corporate.html')
-
-@app.route('/contact.html')
-def contact():
-	return render_template('contact.html')
-
-def main():
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
 
 def build():
 	from flask_frozen import Freezer
---
+++ views.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about.html')
+def about():
+	return render_template('about.html')
+
+@app.route('/calendar.html')
+def calendar():
+	return render_template('calendar.html')
+
+@app.route('/projects.html')
+def projects():
+	return render_template('projects.html')
+
+@app.route('/corporate.html')
+def corporate():
+	return render_template('corporate.html')
+
+@app.route('/contact.html')
+def contact():
+	return render_template('contact.html')
