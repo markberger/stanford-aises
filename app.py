6f2c4d1a8b0be061c52310cc3311a51b63d17abf
Mark Berger <mark.berger.j@gmail.com>
2013-06-13 19:05:17 -0400

    Removed old freeze function

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
 from flask import Flask 
 
 app = Flask(__name__)
-
-def build():
-	from flask_frozen import Freezer
-	freezer = Freezer(app)
-	freezer.freeze()
-
-if __name__ == '__main__':
-	main()
